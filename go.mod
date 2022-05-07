f48298bd999502d9de3e82c06aa657f8076d5340
shapito27 <legionerust@yandex.ru>
2022-05-08 00:42:44 +0800

    add gitignore; use package for reading yaml config;

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module walkthedog
 go 1.18
 
 require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
+	"errors"
+	"io/ioutil"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"gopkg.in/yaml.v2"
 )
 
+const (
+	productionEnv  = "production"
+	testEnv        = "test"
+	developmentEnv = "development"
+)
+
+type EnvironmentConfig map[string]*Config
+
+type Config struct {
+	APIToken string `yaml:"api_token"`
+	Timeout  int    `yaml:"timeout"`
+}
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	env := developmentEnv
+	config, err := getConfig(env)
+	if err != nil {
+		log.Panic(err)
+	}
+	bot, err := tgbotapi.NewBotAPI(config.APIToken)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	if env == developmentEnv {
+		bot.Debug = true
+	}
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	u.Timeout = 20
 
 	updates := bot.GetUpdatesChan(u)
 
@@ -31,4 +54,24 @@ func main() {
 			bot.Send(msg)
 		}
 	}
+}
+
+func getConfig(environment string) (*Config, error) {
+	yamlFile, err := ioutil.ReadFile("telegram.yml")
+	if err != nil {
+		return nil, err
+	}
+	var environmentConfig EnvironmentConfig
+	err = yaml.Unmarshal(yamlFile, &environmentConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	if environmentConfig[environment] == nil {
+		return nil, errors.New("wrong environment set")
+	}
+
+	log.Println(environmentConfig[environment])
+
+	return environmentConfig[environment], nil
 }
